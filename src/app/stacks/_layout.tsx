65db086dc62c71df764607eb6f50fe890c710e3e
pedroso-miguell <miguellarapedroso37@gmail.com>
2024-06-13 11:15:57 -0300

    Página Inicial Concluida

--- src/app/stacks/_layout.tsx
+++ src/app/stacks/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from 'expo-router';
 export default function StackLayout() {
     return (
         <Stack>
-            <Stack.Screen name="inicial" options={{ title: 'Página Inicial' }} />
+            <Stack.Screen name="inicial" options={{ headerShadowVisible: false, headerShown: false}} />
             <Stack.Screen name="login" options={{ title: 'Login' }} />
             <Stack.Screen name="cadastro" options={{ title: 'Cadastro' }} />
         </Stack>
--- src/app/stacks/inicial.tsx
+++ src/app/stacks/inicial.tsx
@@ -1,42 +1,103 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { useRouter } from 'expo-router';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import { useRouter } from "expo-router";
 
 export default function Inicial() {
-    const router = useRouter();
+  const router = useRouter();
 
-    function BtnLogin() {
-        router.push('/stacks/login');
-    }
+  function BtnLogin() {
+    router.push("/stacks/login");
+  }
 
-    function BtnCadastro() {
-        router.push('/stacks/cadastro');
-    }
+  function BtnCadastro() {
+    router.push("/stacks/cadastro");
+  }
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Página Inicial</Text>
-            <View style={styles.buttonContainer}>
-                <Button title="Ir para Login" onPress={BtnLogin} />
-            </View>
-            <View style={styles.buttonContainer}>
-                <Button title="Ir para Cadastro" onPress={BtnCadastro} />
-            </View>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+
+    <View style={styles.introducao}>
+      <Text style={styles.title}>Olá!</Text>
+      <Text style={styles.descricao}>Bem-vindo a GoDress</Text>
+      </View>
+
+      <Image source={require('../imgs/Gzao.svg')} style={styles.logo}/>
+
+      
+      <View style={styles.botoes}>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.buttonLogin} onPress={BtnLogin}>
+          <Text style={styles.TxtBtnLogin}>Faça Login</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.buttonCadastro} onPress={BtnCadastro}>
+          <Text style={styles.TxtBtnCadastro}>Cadastre-se</Text>
+        </TouchableOpacity>
+      </View>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    title: {
-        fontSize: 24,
-        marginBottom: 20,
-    },
-    buttonContainer: {
-        marginVertical: 10,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  introducao: {
+    marginLeft:20,
+    marginTop: '50%',
+  },
+  botoes: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: '45%',
+  },
+  logo: {
+    width: 100, 
+    height: 100, 
+    marginBottom: 20,
+    marginLeft: '70%',
+    marginTop: '50%',
+    position: 'absolute'
+  },
+  title: {
+    fontSize: 60,
+    fontWeight: "bold",
+  },
+  descricao: {
+    fontSize: 18,
+    marginBottom: 10,
+    color: "gray",
+  },
+  buttonContainer: {
+    width: "80%",
+    alignItems: "center",
+    marginTop: 10,
+  },
+  buttonLogin: {
+    width: "100%",
+    backgroundColor: "#593C9D",
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  TxtBtnLogin: {
+    color: "white",
+    fontSize: 20,
+  },
+  buttonCadastro: {
+    width: "100%",
+    backgroundColor: "#593C9D",
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  TxtBtnCadastro: {
+    color: "white",
+    fontSize: 20,
+  },
+
 });
